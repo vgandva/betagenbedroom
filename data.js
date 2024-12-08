ee2a90a20275232c53e7fbcd2257ef3b6a013a37
vgandva <vgandvr@gmail.com>
2024-12-08 10:18:39 +0530

    Add All Files

---
+++ data.js
@@ -0,0 +1,148 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bed-room-1",
+      "name": "Bed Room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5209814256284346,
+        "pitch": 0.00251656057787919,
+        "fov": 1.4150327825709237
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8051197974463395,
+          "pitch": 0.6715302936030589,
+          "rotation": 0,
+          "target": "1-bed-room2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4412779032015806,
+          "pitch": -0.05525468062157479,
+          "title": "Wardrobe laminates",
+          "text": "Satvario"
+        },
+        {
+          "yaw": -1.3354417787425668,
+          "pitch": 0.0695650326632915,
+          "title": "Timber",
+          "text": "Customized timber"
+        },
+        {
+          "yaw": -0.5844944488921122,
+          "pitch": -0.10171121167521058,
+          "title": "glass shutter",
+          "text": "Flutted Glass Shutter"
+        },
+        {
+          "yaw": -0.9210083958592428,
+          "pitch": -0.08097852460600308,
+          "title": "Wardrobe",
+          "text": "Glossy Gray laminated sheet"
+        },
+        {
+          "yaw": -0.03494765469022809,
+          "pitch": 0.12796100097278895,
+          "title": "modern mirror",
+          "text": "Round Mirror with Engraved Light"
+        },
+        {
+          "yaw": 0.42620621400207526,
+          "pitch": 0.021855416063209177,
+          "title": "Room Entrance Door",
+          "text": "Redymade&nbsp;"
+        },
+        {
+          "yaw": 3.0789585953569354,
+          "pitch": 0.4118059983859439,
+          "title": "Study Table",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.4182884892077983,
+          "pitch": 0.05303234357428721,
+          "title": "Bath Room",
+          "text": "ReadyMade Door"
+        },
+        {
+          "yaw": 1.8297107065362939,
+          "pitch": 0.6680959621593772,
+          "title": "tv unit",
+          "text": "white laminate"
+        },
+        {
+          "yaw": 2.5848704815784336,
+          "pitch": 1.347643110559254,
+          "title": "Floor Tiles 2'*2'",
+          "text": "Okra Natural tiles"
+        }
+      ]
+    },
+    {
+      "id": "1-bed-room2",
+      "name": "Bed room2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.7669483913232469,
+        "pitch": 0.062009341809279306,
+        "fov": 1.4150327825709237
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2832823248061125,
+          "pitch": 0.4574072653324883,
+          "rotation": 0,
+          "target": "0-bed-room-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bed Room 1",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
